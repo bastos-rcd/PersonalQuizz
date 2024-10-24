e51368721e5431c375617a72f8a3731ae6fc0865
Bastien <record.bastien@gmail.com>
2024-10-24 21:00:29 +0200

    feat-electron: Home window

---
+++ electron/main.js
@@ -0,0 +1,35 @@
+const { app, BrowserWindow, screen } = require('electron');
+
+let mainWindow;
+
+function createWindow() {
+    const { width, height } = screen.getPrimaryDisplay().workAreaSize;
+
+    mainWindow = new BrowserWindow({
+        width: width,
+        height: height,
+        resizable: false,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        }
+    });
+
+    mainWindow.loadFile('index.html');
+}
+
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
